48c97b01acfb14278c28687e22d72424289a42ac
Thibault Vlacich <thibault.vlacich@gmail.com>
2016-04-20 12:45:32 +0200

    [class Player] Ajout de commentaires

--- src/game/Player.java
+++ src/game/Player.java
@@ -14,12 +14,23 @@ package game;
 import models.Color;
 
 public class Player {
+  // Couleur actuelle du joueur
   private Color color;
 
+  /**
+   * Permet d'obtenir la couleur actuelle du joueur
+   * 
+   * @return  La couleur actuelle du joueur
+   */
   public Color getColor() {
     return color;
   }
 
+  /**
+   * Permet de modifier la couleur actuelle du joueur
+   * 
+   * @param c La nouvelle couleur du joueur
+   */
   public void setColor(Color c) {
     color = c;
   }
